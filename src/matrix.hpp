eacdd54f89861aa4fe9b4a271ba5de107e5fc00a
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2021-12-09 11:49:34 -0800

    fixed the creation of matrices

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -19,7 +19,7 @@ class Matrix{
         row_major = new T[n*m];
         for(int i = 0; i < n; i++){
           for(int j = 0; j < m; j++){
-            row_major[i*3+j]= T(0);
+            row_major[i*m+j]= T(0);
           }
         }
       }
@@ -29,7 +29,7 @@ class Matrix{
         row_major = new T[n_t*m_t];
         for(int i = 0; i < n; i++){
           for(int j = 0; j < m; j++){
-            row_major[i*3+j]= row_majorical[i*3+j];
+            row_major[i*m+j]= row_majorical[i*m+j];
           }
         }
       }
@@ -45,8 +45,25 @@ class Matrix{
     T& at(int i, int j){
       assert(row_major != nullptr);
       assert(i < n && j < m);
-      return row_major[i*n + j];
+      return row_major[i*m + j];
     }
+    
+    bool operator==(const Matrix<T>& rhs){
+      if(n != rhs.n || m != rhs.m){
+        return false;
+      }
+      else{
+        for(int i = 0; i < n; i++){
+          for(int j = 0; j < m; j++){
+            if(!equality(row_major[i*n+j], rhs.row_major[i*n+j])){
+              return false;
+            }
+          }
+        }
+      }
+      return true;
+    }
+
   private:
     T* row_major;
     int n;
--- src/tests/matrix_tests.hpp
+++ src/tests/matrix_tests.hpp
@@ -19,6 +19,38 @@ TEST(TEST_MATRIX, at){
             EXPECT_EQ(i*3+j, A.at(i,j));
         }
     }
+    float b[12];
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            b[i*4+j]= i*4+j;
+        }
+    }
+    Matrix<float> B(b,3,4);
+    for(int i = 0; i < 12; i++){
+        EXPECT_EQ(i, B.at(i));
+    }
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            EXPECT_EQ(i*4+j, B.at(i,j));
+        }
+    }
+    float c[110];
+    int n = 10;
+    int m = 11;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            c[i*m+j]= i*m+j;
+        }
+    }
+    Matrix<float> C(c,n,m);
+    for(int i = 0; i < n*m; i++){
+        EXPECT_EQ(i, C.at(i));
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            EXPECT_EQ(i*m+j, C.at(i,j));
+        }
+    }
 }
 
 /* [[ 1  -1   0]
